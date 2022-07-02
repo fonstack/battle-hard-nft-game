2f8b029fe3df9a6d0cd0bec51b53641cb3797f04
Carlos Fontes <fonstack@gmail.com>
2022-07-02 19:58:04 +0100

    attack boss function started

--- contracts/contracts/BattleHard.sol
+++ contracts/contracts/BattleHard.sol
@@ -10,6 +10,8 @@ import 'hardhat/console.sol';
 import './libraries/Base64.sol';
 
 contract BattleHard is ERC721 {
+    uint256 private randomSeed;
+
     struct CharacterAttributes {
         uint256 characterIndex;
         string name;
@@ -18,6 +20,14 @@ contract BattleHard is ERC721 {
         uint256 maxHp;
         uint256 attackDamage;
         uint256 criticalChance;
+    }
+
+    struct BigBoss {
+        string name;
+        string imageURI;
+        uint256 hp;
+        uint256 maxHp;
+        uint256 attackDamage;
     }
 
     using Counters for Counters.Counter;
@@ -31,13 +41,27 @@ contract BattleHard is ERC721 {
     // NFT owner => NFT tokenId
     mapping(address => uint256) public nftHolders;
 
+    BigBoss public bigBoss;
+
     constructor(
         string[] memory characterNames,
         string[] memory characterImageURIs,
         uint256[] memory characterHps,
         uint256[] memory characterAttackDmgs,
-        uint256[] memory characterCriticalChances
+        uint256[] memory characterCriticalChances,
+        string memory bossName,
+        string memory bossImageURI,
+        uint256 bossHp,
+        uint256 bossAttackDamage
     ) ERC721('Heroes', 'HERO') {
+        // Seed initialization
+        randomSeed = (block.timestamp + block.difficulty) % 100;
+
+        // Boss initialization
+        bigBoss = BigBoss({name: bossName, imageURI: bossImageURI, hp: bossHp, maxHp: bossHp, attackDamage: bossAttackDamage});
+        console.log('Done initializing boss %s w/ HP %s, img %s', bigBoss.name, bigBoss.hp, bigBoss.imageURI);
+
+        // Characters initialization
         for (uint256 i = 0; i < characterNames.length; i += 1) {
             defaultCharacters.push(
                 CharacterAttributes({
@@ -103,7 +127,7 @@ contract BattleHard is ERC721 {
                 strMaxHp,
                 '}, { "trait_type": "Attack Damage", "value": ',
                 strAttackDamage,
-                '}, { "trait_type": "Critical Chance", "value": ',
+                '}, { "trait_type": "Critical Chance", "display_type": "boost_percentage", "value": ',
                 strCriticalChance,
                 '} ]}'
             )
@@ -112,5 +136,48 @@ contract BattleHard is ERC721 {
         string memory output = string(abi.encodePacked('data:application/json;base64,', json));
 
         return output;
+    }
+
+    function attackBoss() public {
+        // Get the state of the player's NFT.
+        uint256 nftTokenIdOfPlayer = nftHolders[msg.sender];
+        CharacterAttributes storage player = nftHolderAttributes[nftTokenIdOfPlayer];
+
+        console.log('\nPlayer w/ character %s about to attack. Has %s HP and %s AD', player.name, player.hp, player.attackDamage);
+        console.log('Boss %s has %s HP and %s AD', bigBoss.name, bigBoss.hp, bigBoss.attackDamage);
+
+        // Make sure the player has more than 0 HP.
+        require(player.hp > 0, 'Error: character must have HP to attack boss.');
+
+        // Make sure the boss has more than 0 HP.
+        require(bigBoss.hp > 0, 'Error: boss must have HP to attack character.');
+
+        // Allow player to attack boss.
+        randomSeed = (block.difficulty + block.timestamp + randomSeed) % 100;
+        bool isCritical = randomSeed <= player.criticalChance;
+        uint256 newPlayerAttackDamage = isCritical ? player.attackDamage * 2 : player.attackDamage;
+
+        if (isCritical) {
+            console.log('Player will attack boss with a critical hit!');
+        }
+
+        if (bigBoss.hp < newPlayerAttackDamage) {
+            bigBoss.hp = 0;
+        } else {
+            bigBoss.hp = bigBoss.hp - newPlayerAttackDamage;
+        }
+
+        // Allow boss to attack player.
+        if (bigBoss.hp > 0) {
+            if (player.hp < bigBoss.attackDamage) {
+                player.hp = 0;
+            } else {
+                player.hp = player.hp - bigBoss.attackDamage;
+            }
+        }
+
+        // Console for ease.
+        console.log("\nPlayer attacked boss. New boss hp: %s", bigBoss.hp);
+        console.log("Boss attacked player. New player hp: %s\n", player.hp);
     }
 }
--- contracts/scripts/run.js
+++ contracts/scripts/run.js
@@ -3,26 +3,27 @@ const main = async () => {
   const gameContract = await gameContractFactory.deploy(
     ['Gwydion', 'Masamba', 'Helve'],
     [
-      'https://firebasestorage.googleapis.com/v0/b/fon-stack.appspot.com/o/others%2Fhero_1.png?alt=media&token=46b48a15-7a38-41a9-a47f-75e65f4cb0ab',
-      'https://firebasestorage.googleapis.com/v0/b/fon-stack.appspot.com/o/others%2Fhero_2.png?alt=media&token=be84c8c1-f6e8-4cdc-9225-c284fa8cd543',
-      'https://firebasestorage.googleapis.com/v0/b/fon-stack.appspot.com/o/others%2Fhero_3.png?alt=media&token=4680e761-9791-4f29-bce4-2f4bdd30b775',
+      'https://firebasestorage.googleapis.com/v0/b/fon-stack.appspot.com/o/others%2Fhero_1.png?alt=media&token=ed479490-1170-468f-bc14-5537896dc27a',
+      'https://firebasestorage.googleapis.com/v0/b/fon-stack.appspot.com/o/others%2Fhero_2.png?alt=media&token=cd1d3dcb-ea6e-4da8-a117-ebef32e5d59d',
+      'https://firebasestorage.googleapis.com/v0/b/fon-stack.appspot.com/o/others%2Fhero_3.png?alt=media&token=6aef186d-cc21-4a84-9038-0a03eb43561e',
     ],
     [300, 200, 100], // HP
     [25, 50, 125], // Attack damage
-    [13, 17, 26] // Critical Chance
+    [13, 17, 26], // Critical Chance
+    'Wynoynk', // Boss name
+    'https://firebasestorage.googleapis.com/v0/b/fon-stack.appspot.com/o/others%2Fmonster_1.png?alt=media&token=c222d657-1968-4fcc-9732-c05fdcb17c5d', // Boss image
+    10000, // Boss hp
+    50 // Boss attack damage
   );
   await gameContract.deployed();
   console.log('Contract deployed to:', gameContract.address);
 
   let txn;
-  // We only have three characters.
-  // an NFT w/ the character at index 2 of our array.
   txn = await gameContract.mintCharacterNFT(2);
   await txn.wait();
 
-  // Get the value of the NFT's URI.
-  let returnedTokenUri = await gameContract.tokenURI(1);
-  console.log('Token URI:', returnedTokenUri);
+  txn = await gameContract.attackBoss();
+  await txn.wait();
 };
 
 const runMain = async () => {
